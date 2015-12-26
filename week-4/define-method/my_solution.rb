edc60b45cf1e3c4d7b8902e252b16a9c59abc0b0
Ryan F. Salerno <ryan@salerno.co>
2015-12-26 16:12:41 -0500

    First commit

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,12 @@
+# Define an Empty Method
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def do_something(one, two, three)
+  #nothing
+end
+
+#do_something(one, two, three)
---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,27 @@
+
+# Math Methods
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ answer = num_1 + num_2
+ return answer
+end
+
+def subtract(num_1, num_2)
+ answer = num_1 - num_2
+ return answer
+end
+
+def multiply(num_1, num_2)
+ answer = num_1 * num_2
+ return answer
+end
+
+def divide(num_1, num_2)
+ answer = num_1 / num_2
+ return answer
+end
